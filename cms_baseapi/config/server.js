630408a30528780226247a4a70a58d498c6b155e
Alex Mai <alex@alexmai.com>
2021-01-31 19:02:44 +0100

    first push
    Upload for basic app install

---
+++ cms_baseapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd9c61b33d49934da94cc4d677e4617a7'),
+    },
+  },
+});
---
+++ cms_baseapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77e50952-9a52-4fc5-b3e1-22ef3ec1610e'
+};
